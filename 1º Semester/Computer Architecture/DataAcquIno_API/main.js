4213cd47073c2505df0887456ff1941281ec81fc
Strilicherk <matheuscherk@hotmail.com>
2025-10-06 15:36:01 -0300

    API Code updated

--- 1º Semester/Computer Architecture/DataAcquIno_API/main.js
+++ 1º Semester/Computer Architecture/DataAcquIno_API/main.js
@@ -8,22 +8,22 @@ const SERIAL_BAUD_RATE = 9600;
 const SERVIDOR_PORTA = 3300;
 
 // habilita ou desabilita a inserção de dados no banco de dados
-const HABILITAR_OPERACAO_INSERIR = false;
+const HABILITAR_OPERACAO_INSERIR = true;
 
 // função para comunicação serial
 const serial = async (
-    valoresSensorAnalogico,
-    valoresSensorDigital,
+    valoresSensorTemperatura,
+   // valoresSensorDigital,
 ) => {
 
     // conexão com o banco de dados MySQL
     let poolBancoDados = mysql.createPool(
         {
-            host: 'HOST_DO_BANCO',
-            user: 'USUARIO_DO_BANCO',
-            password: 'SENHA_DO_BANCO',
-            database: 'DATABASE_DO_BANCO',
-            port: 3306
+            host: '127.0.0.1',
+            user: 'aluno',
+            password: 'Sptech#2024',
+            database: 'StayCold',
+            port: 3307
         }
     ).promise();
 
@@ -51,22 +51,22 @@ const serial = async (
     arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
         console.log(data);
         const valores = data.split(';');
-        const sensorDigital = parseInt(valores[0]);
-        const sensorAnalogico = parseFloat(valores[1]);
+     //   const sensorDigital = parseInt(valores[0]);
+        const sensorTemperatura = parseFloat(valores[0]);
 
         // armazena os valores dos sensores nos arrays correspondentes
-        valoresSensorAnalogico.push(sensorAnalogico);
-        valoresSensorDigital.push(sensorDigital);
+        valoresSensorTemperatura.push(sensorTemperatura);
+       // valoresSensorDigital.push(sensorDigital);
 
         // insere os dados no banco de dados (se habilitado)
         if (HABILITAR_OPERACAO_INSERIR) {
 
             // este insert irá inserir os dados na tabela "medida"
             await poolBancoDados.execute(
-                'INSERT INTO medida (sensor_analogico, sensor_digital) VALUES (?, ?)',
-                [sensorAnalogico, sensorDigital]
+                'INSERT INTO leitura (temperatura) VALUES (?)',
+                [sensorTemperatura]
             );
-            console.log("valores inseridos no banco: ", sensorAnalogico + ", " + sensorDigital);
+            console.log("valores inseridos no banco: ", sensorTemperatura);
 
         }
 
@@ -80,8 +80,8 @@ const serial = async (
 
 // função para criar e configurar o servidor web
 const servidor = (
-    valoresSensorAnalogico,
-    valoresSensorDigital
+    valoresSensorTemperatura,
+   // valoresSensorDigital
 ) => {
     const app = express();
 
@@ -99,28 +99,28 @@ const servidor = (
 
     // define os endpoints da API para cada tipo de sensor
     app.get('/sensores/analogico', (_, response) => {
-        return response.json(valoresSensorAnalogico);
+        return response.json(valoresSensorTemperatura);
     });
-    app.get('/sensores/digital', (_, response) => {
+    /*app.get('/sensores/digital', (_, response) => {
         return response.json(valoresSensorDigital);
-    });
+    });*/
 }
 
 // função principal assíncrona para iniciar a comunicação serial e o servidor web
 (async () => {
     // arrays para armazenar os valores dos sensores
-    const valoresSensorAnalogico = [];
-    const valoresSensorDigital = [];
+    const valoresSensorTemperatura = [];
+    //const valoresSensorDigital = [];
 
     // inicia a comunicação serial
     await serial(
-        valoresSensorAnalogico,
-        valoresSensorDigital
+        valoresSensorTemperatura,
+        //valoresSensorDigital
     );
 
     // inicia o servidor web
     servidor(
-        valoresSensorAnalogico,
-        valoresSensorDigital
+        valoresSensorTemperatura,
+        //valoresSensorDigital
     );
 })();
